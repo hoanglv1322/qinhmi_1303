1640b56236f7b5ae8fdcb389719dcf56f62f6c21
hoang <hoang.lv1322@gmail.com>
2021-09-10 21:33:31 +0700

    update

--- script.js
+++ script.js
@@ -31,83 +31,37 @@ document.addEventListener("DOMContentLoaded", function () {
     return x;
   }
 
-  const carousel = document.querySelector(".carousel__list");
-  const cells = carousel.querySelectorAll(".carousel__cell");
 
-  const cellWidth = carousel.offsetWidth;
-  const cellHeight = carousel.offsetHeight;
-  const cellSize = cellHeight;
-  const cellCount = 21;
 
-  const radius = Math.round(cellSize / 2 / Math.tan(Math.PI / cellCount));
-  const theta = 360 / cellCount;
-
-  var selectedIndex = 0;
-
-  function rotateCarousel() {
-    const angle = theta * selectedIndex * -1;
-    carousel.style.transform =
-      "translateZ(" + -radius + "px) " + "rotateX(" + -angle + "deg)";
-
-    const cellIndex =
-      selectedIndex < 0
-        ? cellCount - ((selectedIndex * -1) % cellCount)
-        : selectedIndex % cellCount;
-
-    const cells = document.querySelectorAll(".carousel__cell");
-    cells.forEach((cell, index) => {
-      if (cellIndex === index) {
-        if (!cell.classList.contains("selected"))
-          cell.classList.add("selected");
-      } else {
-        if (cell.classList.contains("selected")) {
-          cell.classList.remove("selected");
-        }
-      }
+    var swiper = new Swiper(".mySwiper", {
+      effect: "cube",
+      grabCursor: true,
+      cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
     });
-  }
 
-  function selectPrev() {
-    selectedIndex--;
-    rotateCarousel();
-  }
+    var swiper = new Swiper(".mySwiper1", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
 
-  function selectNext() {
-    selectedIndex++;
-    rotateCarousel();
-  }
-
-  var prevButton = document.querySelector(".previous-button");
-  prevButton.addEventListener("click", selectPrev);
-
-  var nextButton = document.querySelector(".next-button");
-  nextButton.addEventListener("click", selectNext);
-
-  function initCarousel() {
-    for (let i = 0; i < cells.length; i++) {
-      const cell = cells[i];
-      const cellAngle = theta * i;
-      cell.style.transform =
-        "rotateX(" + -cellAngle + "deg) translateZ(" + radius + "px)";
-    }
-
-    rotateCarousel();
-  }
-
-  initCarousel();
-});
-
-
-var swiper = new Swiper(".mySwiper", {
-  effect: "cube",
-  grabCursor: true,
-  cubeEffect: {
-    shadow: true,
-    slideShadows: true,
-    shadowOffset: 20,
-    shadowScale: 0.94,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
 });
